071e6bfa74d85a1c55d6da34af88abc6c0948f99
Anand Patel <anandpatel3@hotmail.com>
2019-09-01 22:35:27 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { QuestionControlService } from './superforms/services/question-control.service';
+import { TextboxQuestion } from './superforms/models/questions';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'superforms';
+  questions;
+  constructor(private questionService: QuestionControlService) {
+    this.questions = this.questionService.getQuestions();
+  }
+
+  addAnotherTextInput(name: string) {
+    // const newInputControl = this.fb.control('');
+    // this.questions = this.questionService.toFormGroup([...this.questions, new TextboxQuestion()]);
+    this.questions = [...this.questions, new TextboxQuestion({key: 'helloWorld', type: '', label: 'hello world'})];
+    // this.form.addControl(name, newInputControl);
+  }
+
+  onSubmit(formPayload: string) {
+    console.log(formPayload);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { SuperformsModule } from './superforms/superforms.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+const appRoutes: Routes[] = [
+
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // RouterModule.forRoot(
+    //   appRoutes,
+    //   { enableTracing: true }
+    // ),
+    NgbModule,
+    SuperformsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
